8d9f2ff5f7bbb9734a62b3336a9f211653b0d613
Marc Robison <marc.robison@etu.umontpellier.fr>
2025-02-12 00:44:59 +0100

    endcode and decode functions

--- client.py
+++ client.py
@@ -3,24 +3,5 @@ import socket
 clientsocket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientsocket1.connect(('localhost', 8000))
 
-#clientsocket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#clientsocket2.connect(('localhost', 8001))
-
-#clientsocket3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#clientsocket3.connect(('localhost', 8002))
-
-msg = "Hello World!"
-e_msg = msg.encode()
-
-clientsocket1.send(e_msg)
-reply1 = clientsocket1.recv(64)
-
-#clientsocket2.send(e_msg)
-#reply2 = clientsocket2.recv(64)
-
-#clientsocket3.send(e_msg)
-#reply3 = clientsocket3.recv(64)
-
-print(f"Server replied : {reply1}")
-#print(f"Server replied : {reply2}")
-#print(f"Server replied : {reply3}")
+clientsocket1.send("Hello World!".encode())
+print(f"Server replied : {clientsocket1.recv(64).decode()}")
--- server2.py
+++ server2.py
@@ -12,20 +12,19 @@ while True:
     s_sock.listen(5) # become a server socket, maximum 5 connections
 
     con, addr = s_sock.accept()
-    buf = con.recv(64)
-    print(f"Connection from port {str(con.getsockname()[1])}\nmsg : {buf}")
-    reply = "hi there"
-    con.send(reply.encode())
+
+    print(f"Connection from port {str(con.getsockname()[1])}\nmsg : {con.recv(64).decode()}")
+    con.send("Hi there !".encode())
     con.close()
 
     time.sleep(3)
 
     c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     c_sock.connect(('localhost', 8000))
-    msg = "Hello from server 2!"
-    e_msg = msg.encode()
 
-    c_sock.send(e_msg)
-    reply1 = c_sock.recv(64)
-    c_sock.close
+
+    c_sock.send("Hello from server 0!".encode())
+    #reply1 = c_sock.recv(64)
+    c_sock.close()
+
 
